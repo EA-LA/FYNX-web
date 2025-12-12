e8d226a92aed0ce864f5ca00fcf41b2150e5aca9
Elhamamini <ha6876122@gmail.com>
2025-12-12 11:52:39 -0700

    Create firebase-init.js

---
+++ ssets/css/assets/js/assets/js/firebase-init.js
@@ -0,0 +1,17 @@
+// Firebase init (GitHub Pages friendly, no build tools)
+// IMPORTANT: Fill firebaseConfig from your Firebase Console -> Project settings.
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "PASTE_API_KEY",
+  authDomain: "PASTE_AUTH_DOMAIN",
+  projectId: "PASTE_PROJECT_ID",
+  storageBucket: "PASTE_STORAGE_BUCKET",
+  messagingSenderId: "PASTE_SENDER_ID",
+  appId: "PASTE_APP_ID",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
